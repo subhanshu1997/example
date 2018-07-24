6e8cc06bef2aaffa4260859f474bf83085c2088b
Subhanshu Bigasia <bigasiasubhanshu@ymail.com>
2018-07-25 04:42:22 +0530

    First Commit

---
+++ src/HelloServlet.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+
+public class HelloServlet extends GenericServlet {
+	@Override
+	public void service(ServletRequest req, ServletResponse res)
+			throws ServletException, IOException {
+	res.setContentType("text/html");
+	PrintWriter pw=res.getWriter();
+	pw.println("Hello!");
+	pw.close();
+	}
+
+}
